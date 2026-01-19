e41088ba7189c10b3513f04effad46fee7591d6f
Aaron Patterson <tenderlove@ruby-lang.org>
2026-01-19 14:36:04 -0800

    fix decoding tests

--- test/decode_test.rb
+++ test/decode_test.rb
@@ -9,19 +9,21 @@ module QOI
       define_method("test_decode_#{name}_file") do
         png = ChunkyPNG::Image.from_file(File.join(IMGS, name + ".png"))
         buff = QOI::Buffer.from_file File.join(IMGS, name + ".qoi")
+        expected = buff.channels == 3 ? png.to_rgb_stream : png.to_rgba_stream
 
         assert_equal png.width, buff.width
         assert_equal png.height, buff.height
-        assert_equal png.to_rgba_stream, buff.buffer
+        assert_equal expected, buff.buffer
       end
 
       define_method("test_decode_#{name}_buffer") do
         png = ChunkyPNG::Image.from_file(File.join(IMGS, name + ".png"))
         buff = QOI::Buffer.from_buffer File.binread File.join(IMGS, name + ".qoi")
+        expected = buff.channels == 3 ? png.to_rgb_stream : png.to_rgba_stream
 
         assert_equal png.width, buff.width
         assert_equal png.height, buff.height
-        assert_equal png.to_rgba_stream, buff.buffer
+        assert_equal expected, buff.buffer
       end
     end
   end
